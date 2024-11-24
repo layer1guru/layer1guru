bc41e19fd4b18880ec67d29533642a059395417c
Shomari <nyusternie@sdot.io>
2024-11-23 19:50:40 -0500

    Update hero msg.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -282,7 +282,7 @@ export default async function Home() {
             <Container className="mt-9">
                 <div className="max-w-2xl">
                     <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-                        Learn to Design, Launch &amp; Manage <span className="italic text-sky-400">LIMITLESS</span> L1s — then realize your <span className="italic text-fuchsia-400">WILDEST</span> dreams!
+                        Learn to Design, Launch &amp; Manage <span className="italic text-sky-400">LIMITLESS</span> L1s — let's realize your <span className="italic text-fuchsia-400">WILDEST</span> dreams!
                     </h1>
 
                     <p className="mt-6 text-base text-zinc-600 dark:text-zinc-300 leading-8 tracking-wider">
