44692d25ece82b2324c8189deca567d4b06fa1a7
Shomari <nyusternie@sdot.io>
2024-12-18 13:31:22 -0500

    Add plausible analytics.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,34 +1,42 @@
 import { type Metadata } from 'next'
 
+import PlausibleProvider from 'next-plausible'
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s - L1 GÜRŲ',
-    default:
-      'L1 GÜRŲ - Software designer, founder, and amateur astronaut',
-  },
-  description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    title: {
+        template: '%s - L1 GÜRŲ',
+        default:
+            'L1 GÜRŲ - Software designer, founder, and amateur astronaut',
+    },
+    description:
+        'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body className="flex h-full bg-zinc-50 dark:bg-black">
-        <Providers>
-          <div className="flex w-full">
-            <Layout>{children}</Layout>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+            <body className="flex h-full bg-zinc-50 dark:bg-black">
+                <PlausibleProvider
+                    domain="layer1.guru"
+                    customDomain="https://plausible.layer1.guru"
+                >
+                    <Providers>
+                        <div className="flex w-full">
+                            <Layout>
+                                {children}
+                            </Layout>
+                        </div>
+                    </Providers>
+                </PlausibleProvider>
+            </body>
+        </html>
+    )
 }
