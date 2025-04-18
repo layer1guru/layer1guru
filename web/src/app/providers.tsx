b3dd6e1401ae50c68ef6a5d1d3e769ccc8a75e0c
Shomari <nyusternie@sdot.io>
2025-04-18 11:53:24 -0400

    Fix providers.

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -5,51 +5,52 @@ import { usePathname } from 'next/navigation'
 import { ThemeProvider, useTheme } from 'next-themes'
 
 function usePrevious<T>(value: T) {
-  let ref = useRef<T>()
+    const ref = useRef<T>(undefined)
 
-  useEffect(() => {
-    ref.current = value
-  }, [value])
+    useEffect(() => {
+        ref.current = value
+    }, [value])
 
-  return ref.current
+    return ref.current
 }
 
 function ThemeWatcher() {
-  let { resolvedTheme, setTheme } = useTheme()
+    const { resolvedTheme, setTheme } = useTheme()
 
-  useEffect(() => {
-    let media = window.matchMedia('(prefers-color-scheme: dark)')
+    useEffect(() => {
+        const media = window.matchMedia('(prefers-color-scheme: dark)')
 
-    function onMediaChange() {
-      let systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('system')
-      }
-    }
+        function onMediaChange() {
+            const systemTheme = media.matches ? 'dark' : 'light'
 
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
+            if (resolvedTheme === systemTheme) {
+                setTheme('system')
+            }
+        }
 
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
-  }, [resolvedTheme, setTheme])
+        onMediaChange()
+        media.addEventListener('change', onMediaChange)
 
-  return null
+        return () => {
+            media.removeEventListener('change', onMediaChange)
+        }
+    }, [resolvedTheme, setTheme])
+
+    return null
 }
 
 export const AppContext = createContext<{ previousPathname?: string }>({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  let pathname = usePathname()
-  let previousPathname = usePrevious(pathname)
+    const pathname = usePathname()
+    const previousPathname = usePrevious(pathname)
 
-  return (
-    <AppContext.Provider value={{ previousPathname }}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <ThemeWatcher />
-        {children}
-      </ThemeProvider>
-    </AppContext.Provider>
-  )
+    return (
+        <AppContext.Provider value={{ previousPathname }}>
+            <ThemeProvider attribute="class" disableTransitionOnChange>
+                <ThemeWatcher />
+                {children}
+            </ThemeProvider>
+        </AppContext.Provider>
+    )
 }
